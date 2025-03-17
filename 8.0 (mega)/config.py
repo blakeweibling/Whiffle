0ed5cd8c1c69a4f3f5d368d0ee5841046189ed13
blakeweibling <bweibling@me.com>
2025-03-17 03:27:44 -0700

    8.0 I am stopping at 10 lol

---
+++ 8.0 (mega)/config.py
@@ -0,0 +1,39 @@
+# config.py
+COLORS = {
+    "BLACK": (0, 0, 0),
+    "WHITE": (255, 255, 255),
+    "GREEN": (0, 255, 0),
+    "RED": (255, 0, 0),
+    "BLUE": (0, 0, 255),
+    "YELLOW": (255, 255, 0),
+    "GRAY": (128, 128, 128),
+    "DARK_GRAY": (50, 50, 50),
+    "LIGHT_GRAY": (100, 100, 100),
+    "BUTTON_BORDER": (0, 0, 0),
+    "SUBMENU_BG": (50, 50, 50, 200),
+}
+
+SETTINGS = {
+    "INITIAL_WIDTH": 1280,
+    "INITIAL_HEIGHT": 720,
+    "COOLDOWN_FRAMES": 30,
+    "CONFIRMATION_FRAMES": 10,
+    "SOUND_COOLDOWN": 1.0,
+    "MENU_HEIGHT": 60,
+    "STATUS_BAR_HEIGHT": 50,
+    "BACKGROUND_MUSIC_FILE": "background_music.mp3",
+    "SCORE_SOUND_FILE": "score.wav",
+    "SPLASH_IMAGE_FILE": "whiffle_splash.jpg",
+    "CALIBRATION_FILE": "calibration.json",
+}
+
+FONT_SIZES = {
+    "MENU": 30,
+    "UI": 36,
+    "WINDOW": 24,
+}
+
+SUPABASE = {
+    "URL": "https://jtkbujumrobglftzokcs.supabase.co",
+    "KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0a2J1anVtcm9iZ2xmdHpva2NzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwMTM4NzcsImV4cCI6MjA1NzU4OTg3N30.OibLuqr3X922SUSBL8yGxDw8uwuTjivH97-2wNhJDqs",
+}
